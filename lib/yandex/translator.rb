f00b4569490e7d055c991468817cd6441208eaaf
Roarsa <43239215+Roarsa@users.noreply.github.com>
2018-11-27 20:29:56 +0300

    Add files via upload

--- lib/yandex/translator.rb
+++ lib/yandex/translator.rb
@@ -4,45 +4,51 @@ require "json"
 
 module Yandex
   
- 	class ApiError < StandardError
- 	end
+    class ApiError < StandardError
+    end
 
-	class Translator
-		def initialize(api_key)
-			@api_key = api_key
-		end
+    class Translator
+        def initialize(api_key)
+            @api_key = api_key
+            @api_url = "https://translate.yandex.net/api/v1.5/tr.json"
+        end
 
-		def get_languages(ui)
-			uri = URI.parse("https://translate.yandex.net/api/v1.5/tr.json/getLangs?key=#{@api_key}&ui=#{ui}")
-			response = Net::HTTP.get(uri)
-			if JSON.parse(response)["code"] == 401 || JSON.parse(response)["code"] == 402
-				raise ApiError.new(JSON.parse(response)["message"])
-			else
-				JSON.parse(response)
-			end
-		end
+        def uri(method, ui = '') 
+          case method
+          when 'getLangs'
+            URI.parse("#{@api_url}/#{method}?key=#{@api_key}&ui=#{ui}")
+          else
+            URI.parse("#{@api_url}/#{method}?key=#{@api_key}&")
+          end
+        end
 
-		def define_languages(text, hint='')
-			uri = URI.parse("https://translate.yandex.net/api/v1.5/tr.json/detect?key=#{@api_key}&")
-			parameters =  {"text" => text, "hint" => hint}
-			response = Net::HTTP.post_form(uri, parameters)
-			if response.code== "200"
-				JSON.parse(response.body)["lang"]
-			else
-				raise ApiError.new(JSON.parse(response.body)["message"])
-			end
-		end
+        def post(ui, parameters) 
+          response = Net::HTTP.post_form(ui, parameters)
+          if response.code == "200"
+              JSON.parse(response.body)
+          else
+              raise ApiError.new(JSON.parse(response.body)["message"])
+          end
+        end
 
-		def translate_text(text, final_lang, lang=nil,format='plain')
-			uri = URI.parse("https://translate.yandex.net/api/v1.5/tr.json/translate?key=#{@api_key}&")
-			language = lang ? "#{lang}-#{final_lang}" : final_lang
-			parameters =  {"text" => text, "lang" => language, "format" => format}
-			response = Net::HTTP.post_form(uri, parameters)
-			if response.code == "200"
-				JSON.parse(response.body)["text"][0]
-			else
-				raise ApiError.new(JSON.parse(response.body)["message"])
-			end
-		end
-	end
+        def get_languages(ui)
+            response = Net::HTTP.get(uri('getLangs', ui))
+            if JSON.parse(response)["code"] == 401 || JSON.parse(response)["code"] == 402
+                raise ApiError.new(JSON.parse(response)["message"])
+            else
+                JSON.parse(response)
+            end
+        end
+
+        def define_languages(text, hint='')
+            parameters =  {"text" => text, "hint" => hint}
+            post(uri('detect'), parameters)["lang"]
+        end
+
+        def translate_text(text, final_lang, lang=nil,format='plain')
+            language = lang ? "#{lang}-#{final_lang}" : final_lang
+            parameters =  {"text" => text, "lang" => language, "format" => format}
+            post(uri('translate'), parameters)["text"][0]
+        end
+    end
 end
