834d8f2087874e607a7fa9f9d03e8a9243952b07
Roarsa <43239215+Roarsa@users.noreply.github.com>
2018-12-03 13:32:47 +0300

    Add files via upload

--- lib/yandex/translator.rb
+++ lib/yandex/translator.rb
@@ -13,15 +13,6 @@ module Yandex
             @api_url = "https://translate.yandex.net/api/v1.5/tr.json"
         end
 
-        def uri(method, ui = '') 
-          case method
-          when 'getLangs'
-            URI.parse("#{@api_url}/#{method}?key=#{@api_key}&ui=#{ui}")
-          else
-            URI.parse("#{@api_url}/#{method}?key=#{@api_key}&")
-          end
-        end
-
         def post(ui, parameters) 
           response = Net::HTTP.post_form(ui, parameters)
           if response.code == "200"
@@ -30,25 +21,43 @@ module Yandex
               raise ApiError.new(JSON.parse(response.body)["message"])
           end
         end
+    end
 
-        def get_languages(ui)
-            response = Net::HTTP.get(uri('getLangs', ui))
-            if JSON.parse(response)["code"] == 401 || JSON.parse(response)["code"] == 402
-                raise ApiError.new(JSON.parse(response)["message"])
-            else
-                JSON.parse(response)
-            end
-        end
+    class GetLanguages < Translator
+      def uri(ui)
+        URI.parse("#{@api_url}/getLangs?key=#{@api_key}&ui=#{ui}")
+      end
 
-        def define_languages(text, hint='')
-            parameters =  {"text" => text, "hint" => hint}
-            post(uri('detect'), parameters)["lang"]
-        end
+      def main(ui)
+        response = Net::HTTP.get(uri(ui))
+          if JSON.parse(response)["code"] == 401 || JSON.parse(response)["code"] == 402
+              raise ApiError.new(JSON.parse(response)["message"])
+          else
+              JSON.parse(response)
+          end
+      end
+    end        
 
-        def translate_text(text, final_lang, lang=nil,format='plain')
-            language = lang ? "#{lang}-#{final_lang}" : final_lang
-            parameters =  {"text" => text, "lang" => language, "format" => format}
-            post(uri('translate'), parameters)["text"][0]
-        end
+    class TranslateText < Translator
+      def uri
+        URI.parse("#{@api_url}/translate?key=#{@api_key}&")
+      end
+
+      def main(text, final_lang, lang=nil,format='plain')
+          language = lang ? "#{lang}-#{final_lang}" : final_lang
+          parameters =  {"text" => text, "lang" => language, "format" => format}
+          post(uri(), parameters)["text"][0]
+      end
+    end
+
+    class DefineLang < Translator
+      def uri
+        URI.parse("#{@api_url}/detect?key=#{@api_key}&")
+      end
+
+      def main(text, hint='')
+          parameters =  {"text" => text, "hint" => hint}
+          post(uri(), parameters)["lang"]
+      end
     end
 end
